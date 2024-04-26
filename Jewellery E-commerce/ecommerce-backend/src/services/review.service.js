f3652a7102bfa1af42c9c9aa8080af668ac3810a
Kishan Dabhi <dabhikishan358@gmail.com>
2024-04-26 20:00:34 +0530

    mistaken commit

--- Jewellery E-commerce/ecommerce-backend/src/services/review.service.js
+++ Jewellery E-commerce/ecommerce-backend/src/services/review.service.js
@@ -1,7 +1,6 @@
 const Review = require('../models/review.model');
 const productService = require('../services/product.service.js');
 
-
 async function createReview(reqData, user) {
     try {
         const product = await productService.findProductById(reqData.productId);
@@ -11,7 +10,7 @@ async function createReview(reqData, user) {
             product: product._id,
             review: reqData.review,
             createdAt: new Date(),
-        })
+        });
 
         const createdReview = await review.save();
         product.reviews.push(createdReview._id);
@@ -19,17 +18,20 @@ async function createReview(reqData, user) {
 
         return createdReview;
     } catch (error) {
-        throw new Error(error.message);
+        throw new Error(`Error while creating review: ${error.message}`);
     }
 }
 
 async function getAllReview(productId) {
-    // const product = await productService.findProductById(reqData.productId);
-
-    return await Review.find({ product: productId }).populate("user");
+    try {
+        const reviews = await Review.find({ product: productId }).populate("user");
+        return reviews;
+    } catch (error) {
+        throw new Error(`Error while fetching reviews: ${error.message}`);
+    }
 }
 
 module.exports = {
     createReview,
     getAllReview
-}
+};
