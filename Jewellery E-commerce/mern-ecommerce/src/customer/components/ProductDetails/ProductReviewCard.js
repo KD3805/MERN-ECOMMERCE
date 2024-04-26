cf0bf418522fbc4ddea3f7be2ff0ed82d1f235d1
Kishan Dabhi <dabhikishan358@gmail.com>
2024-04-27 01:36:23 +0530

    clean code

--- Jewellery E-commerce/mern-ecommerce/src/customer/components/ProductDetails/ProductReviewCard.js
+++ Jewellery E-commerce/mern-ecommerce/src/customer/components/ProductDetails/ProductReviewCard.js
@@ -14,30 +14,30 @@ const StyledRating = styled(Rating)({
 
 const ProductReviewCard = ({ reviewData, ratingData }) => {
 
-    // const formattedDate = format(reviewData.createdAt, 'MMMM d, yyyy');
-    const formattedDate = reviewData.createdAt && isValid(new Date(reviewData.createdAt)) ? format(new Date(reviewData.createdAt), 'MMMM d, yyyy') : '';
+  // const formattedDate = format(reviewData.createdAt, 'MMMM d, yyyy');
+  const formattedDate = reviewData.createdAt && isValid(new Date(reviewData.createdAt)) ? format(new Date(reviewData.createdAt), 'MMMM d, yyyy') : '';
 
 
   return (
     <div className='my-3'>
       <Grid container gap={3}>
         <Box>
-            <Avatar className='text-white' sx={{width: 50, height: 50, fontSize: '1.3rem', bgcolor: '#832729'}}>
-                {reviewData.user?.firstName.charAt(0).toUpperCase() ?? ''}
-            </Avatar>
+          <Avatar className='text-white' sx={{ width: 50, height: 50, fontSize: '1.3rem', bgcolor: '#832729' }}>
+            {reviewData.user?.firstName.charAt(0).toUpperCase() ?? ''}
+          </Avatar>
         </Box>
 
         <Grid item xs={9}>
-            <div className="mb-2">
-                <div>
-                    <h1 className='font-semibold text-xl m-0'>{reviewData.user?.firstName}</h1>
-                    <p className='font-medium text-sm text-gray-500'>{formattedDate}</p>
-                </div>
+          <div className="mb-2">
+            <div>
+              <h1 className='font-semibold text-xl m-0'>{reviewData.user?.firstName}</h1>
+              <p className='font-medium text-sm text-gray-500'>{formattedDate}</p>
             </div>
+          </div>
 
-            <StyledRating name="rating" value={ratingData?.rating} readOnly precision={0.5}/>
+          <StyledRating name="rating" value={ratingData?.rating} readOnly precision={0.5} />
 
-            <p>{reviewData.review}</p>
+          <p>{reviewData.review}</p>
         </Grid>
 
       </Grid>
