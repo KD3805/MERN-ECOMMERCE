1765216db97183ae5867d929d7e7bc912ff6efa7
Kishan Dabhi <dabhikishan358@gmail.com>
2024-04-26 17:36:46 +0530

    final touch

--- Jewellery E-commerce/mern-ecommerce/src/index.js
+++ Jewellery E-commerce/mern-ecommerce/src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import   { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { store } from './state/store';
+import { Helmet } from 'react-helmet'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
+        <Helmet>
+            <title>Gayatri Fashion Jewelry</title>
+            <meta name='description' content='Discover a dazzling online jewelry shopping experience with gayatri 💎✨' />
+            <meta name="keywords" content="Exquisite Jewelry, Online Store, Luxury Accessories, Secure Transactions, Immersive Design" /> 
+        </Helmet>
         <Provider store={store}>
             <App />
         </Provider>
