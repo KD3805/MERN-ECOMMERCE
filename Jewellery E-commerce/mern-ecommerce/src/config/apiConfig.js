4582b384342b1b41d5bc011b97bfc0010181f69c
Kishan Dabhi <114673669+KD3805@users.noreply.github.com>
2024-04-28 12:58:32 +0530

    Update apiConfig.js

--- Jewellery E-commerce/mern-ecommerce/src/config/apiConfig.js
+++ Jewellery E-commerce/mern-ecommerce/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454";
+export const API_BASE_URL = "https://ecom-backend-meal.onrender.com";
 const jwt = localStorage.getItem("jwt");
 
 export const api = axios.create({
